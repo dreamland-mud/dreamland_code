9e83c0ed21125e393ae595e560799f0969db7337
Ruffina Koza <ruffina.koza@gmail.com>
2019-02-25 12:44:10 +1300

    Fenia API improvements:
    * Write helps for all methods in ObjectWrapper
    * Group all fields together marking them as ro/rw

--- plug-ins/feniaroot/nativeext.h
+++ plug-ins/feniaroot/nativeext.h
@@ -6,6 +6,7 @@
 #define NATIVEEXT_H
 
 #include <sstream>
+#include <iomanip>
 
 #include "native.h"
 
@@ -14,14 +15,40 @@ using namespace std;
 // MOC_SKIP_BEGIN
 namespace Scripting {
 
+struct api_details {
+    string help;
+    bool readOnly;
+};
+
+typedef map<string, api_details> API;
+
 template <typename TT>
 void traitsAPIAux( ostringstream &buf ) 
 {
     typename TT::List *list = TT::List::begin();
     
-    for ( ; list; list = list->getNext())
-        buf << "{g" << list->getKey().name 
-            << "{x: " << list->getVal().help << endl;
+    for ( ; list; list = list->getNext()) {
+        const string &name = list->getKey().name;
+        const string &help = list->getVal().help;
+
+        if (help.size() > 0 && help.at(0) == '(')
+            buf << "{g" << name << "{w" << help << endl;
+        else
+            buf << "{g" << name << "{w: " << help << endl;
+    }
+}
+
+template <typename TT>
+void traitsAPIAux( API &api, bool readOnly ) 
+{
+    typename TT::List *list = TT::List::begin();
+    
+    for ( ; list; list = list->getNext()) {
+        struct api_details d;
+        d.help = list->getVal().help;
+        d.readOnly = readOnly;
+        api[list->getKey().name] = d;
+    }
 }
 
 template <typename T>
@@ -31,11 +58,18 @@ void traitsAPI( ostringstream &buf )
     
     buf << "{Y" << Traits::NAME << "{x" << endl
         << Traits::HELP << endl;
-    
-    buf << endl << "{WПоля, доступные для записи: {x" << endl;
-    traitsAPIAux<typename Traits::Set>( buf );
-    buf << endl << "{WПоля, доступные для чтения: {x" << endl;
-    traitsAPIAux<typename Traits::Get>( buf );
+
+    API api;
+    buf << endl << "{WПоля, доступные только для чтения [ro] и для записи [rw]: {x" << endl;
+
+    traitsAPIAux<typename Traits::Get>( api, true );
+    traitsAPIAux<typename Traits::Set>( api, false );
+
+    for (API::const_iterator a = api.begin(); a != api.end(); a++)
+        buf << "[" << (a->second.readOnly ? "ro" : "rw") << "] "
+            << "{g" << setiosflags(ios::left) << setw(18) << a->first << "{x: " << resetiosflags(ios::left)
+            << a->second.help << endl;
+
     buf << endl << "{WМетоды: {x" << endl;
     traitsAPIAux<typename Traits::Invoke>( buf );
 }
