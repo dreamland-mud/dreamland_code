eb3b11d2f55c3f0340a0f85824314ac8f527d82f
Ruffina Koza <ruffina.koza@gmail.com>
2019-07-07 19:30:54 +1200

    Add convenience methods to StringList.

--- src/lang/stringlist.h
+++ src/lang/stringlist.h
@@ -6,8 +6,24 @@
 
 class StringList : public std::vector<DLString> {
 public:
+    StringList();
     StringList(const DLString &);
+
+    /**
+     * Checks if smaller list elements are prefixes for some elements of this list.
+     */
     bool superListOf(const StringList& smallerList) const;
+
+    /**
+     * Concatenates all elements of the list using delimiter.
+     */
+    DLString join(const DLString &delim) const;
+
+    /**
+     * Returns new list, containing all elements 'wrapped' on both sides
+     * with provided strings.
+     */
+    StringList wrap(const DLString &before, const DLString &after) const;
 };
 
 #endif
