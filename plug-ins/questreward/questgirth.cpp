798134c296662c9f1faa1af35e7275dfa0683bf4
Ruffina Koza <ruffina.koza@gmail.com>
2019-10-09 10:34:13 +1300

    New formula for quest girth.

--- plug-ins/questreward/questgirth.cpp
+++ plug-ins/questreward/questgirth.cpp
@@ -20,96 +20,178 @@ void QuestGirth::wear( Character *ch )
     ch->send_to( "{CТвой пояс ярко вспыхивает.{x\r\n" );
 }
 
+
 void QuestGirth::equip( Character *ch ) 
 {
-    Affect *paf;
-    Affect af;
-
     obj->level = ch->getRealLevel( );
     
     if( obj->affected ) {
-        for( paf = obj->affected; paf; paf = paf->next )
-            addAffect( ch, paf );
+        // Updated existing affects to match player level.
+        Affect *paf;
+        for (paf = obj->affected; paf; paf = paf->next)
+            addAffect(ch, paf);
     }
     else {
+        // Add affects for the first time.
+        static const int applies [] = {
+            APPLY_INT, APPLY_WIS, APPLY_CON, APPLY_DEX, APPLY_STR, 
+            APPLY_AC, APPLY_HIT, APPLY_MANA, APPLY_MOVE,
+            APPLY_HITROLL, APPLY_DAMROLL, -1
+        };
+
+        Affect af;
+
         af.where = TO_OBJECT;
         af.type  = -1;
         af.duration = -1;
         af.bitvector = 0;
 
-        af.location = APPLY_CON;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_AC;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_HIT;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_MANA;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_MOVE;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_HITROLL;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_DAMROLL;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
+        for (int i = 0; applies[i] != -1; i++) {
+            af.location = applies[i];
+            addAffect(ch, &af);
+            affect_to_obj(obj, &af);
+        }
     }
 }
 
 void QuestGirth::addAffect( Character *ch, Affect *paf ) 
 {
-  short level = ch->getModifyLevel();
+    short level = ch->getModifyLevel();
+    bool caster = ch->getTrueProfession( )->getFlags( ).isSet(PROF_CASTER);
+    bool fighter = !caster;
+    bool evil = IS_EVIL(ch);
+    bool good = IS_GOOD(ch);
+    bool neutral = IS_NEUTRAL(ch);
 
-  switch( paf->location ) {
-    case APPLY_DAMROLL:
-      paf->level = level;
-      paf->modifier = IS_EVIL( ch ) ? ( level / 10 ) :
-                        IS_NEUTRAL( ch ) ? ( level / 20 ) : ( level / 40 );
-      if (!ch->getTrueProfession( )->getFlags( ).isSet(PROF_CASTER)) // Bonus for battle clases
-          paf->modifier += paf->modifier/2;
-      return;
-    case APPLY_HITROLL:
-      paf->level = level;
-      paf->modifier = IS_EVIL( ch ) ? ( level / 40 ) :
-                        IS_NEUTRAL( ch ) ? ( level / 20 ) : ( level / 10 );
-      if (!ch->getTrueProfession( )->getFlags( ).isSet(PROF_CASTER)) // Bonus for battle clases
-          paf->modifier += paf->modifier/2;
-      return;
-    case APPLY_HIT:
-      paf->level = level;
-      paf->modifier = level * 2;
-      return;
-    case APPLY_MANA:
-      paf->level = level;
-      paf->modifier = level * 2;
-      if (ch->getTrueProfession( )->getFlags( ).isSet(PROF_CASTER)) // Bonus for custer clases
-           paf->modifier += paf->modifier*3/2;
+    int mod = 0;
 
-      return;
-    case APPLY_MOVE:
-      paf->level = level;
-      paf->modifier = level;
-      return;
-    case APPLY_CON:
-      paf->level = level;
-      paf->modifier = 2;
-      return;
-    case APPLY_AC:
-      paf->level = level;
-      paf->modifier = IS_GOOD( ch ) ? -( level * 3 ) :
-                        IS_EVIL( ch ) ? -( level * 3 ) / 2 : -( level * 2 );
-      return;
-  }
+    switch( paf->location ) {
+        case APPLY_DAMROLL:
+            if (fighter) { // battle class
+                if (evil)
+                    mod = level / 10 + 3;
+                else if (neutral)
+                    mod = level / 10 + 2;
+                else
+                    mod = level / 10 + 1;
+              
+            } else { // caster class
+                if (evil)
+                    mod = level / 10;
+                else if (neutral)
+                    mod = level / 12;
+                else
+                    mod = level / 14;            
+            }
+            break;
+
+        case APPLY_HITROLL:
+            if (fighter) { // battle class
+                if (evil)
+                    mod = level / 10;
+                else if (neutral)
+                    mod = level / 10 + 2;
+                else
+                    mod = level / 10 + 3;
+              
+            } else { // caster class
+                if (evil)
+                    mod = level / 14;
+                else if (neutral)
+                    mod = level / 12;
+                else
+                    mod = level / 10;            
+            }
+            break;
+
+
+        case APPLY_HIT:
+            if (fighter) { // battle class
+                if (evil)
+                    mod = level / 2;
+                else if (neutral)
+                    mod = 3 * level / 4;
+                else
+                    mod = level;
+              
+            } else { // caster class
+                if (evil)
+                    mod = level;
+                else if (neutral)
+                    mod = level * 2;
+                else
+                    mod = level * 3;            
+            }
+            break;
+
+        case APPLY_MANA:
+            if (fighter)  // battle class
+                mod = level;
+            else // caster class
+                mod = level * 5;
+            break;
+
+        case APPLY_MOVE:
+            mod = level;
+            break;
+
+        case APPLY_AC:
+            // Old formula, doesn't make much difference.
+            mod = IS_GOOD( ch ) ? -( level * 3 ) :
+                              IS_EVIL( ch ) ? -( level * 3 ) / 2 : -( level * 2 );
+            break;
+
+        case APPLY_INT:
+            if (caster) {
+                if (evil)
+                    mod = 2;
+                else if (neutral)
+                    mod = 1;
+            }
+            break;
+
+        case APPLY_WIS:
+            if (caster) {
+                if (neutral)
+                    mod = 1;
+                else if (good)
+                    mod = 2;
+            }
+            break;
+
+        case APPLY_DEX:
+            if (fighter) {
+                if (neutral)
+                    mod = 2;
+            }
+            break;
+
+        case APPLY_STR:
+            if (fighter) {
+                if (evil)
+                    mod = 2;
+            }
+            break;
+
+        case APPLY_CON:
+            if (fighter) { 
+                if (evil)
+                    mod = 1;
+                else if (neutral)
+                    mod = 1;
+                else // good fighter
+                    mod = 3;
+
+            } else { // all casters
+                mod = 1; 
+            }
+            break;
+
+        default:
+            return;
+    }
+
+    paf->level = level;
+    paf->modifier = mod;
 }
 
