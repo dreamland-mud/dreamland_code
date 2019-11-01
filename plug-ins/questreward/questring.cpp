20d8fddbdde81680bf81a534a85b8be3d6d04e6a
Ruffina Koza <ruffina.koza@gmail.com>
2019-11-01 13:06:42 +1300

    New formula for quest ring by Taiphoen.

--- plug-ins/questreward/questring.cpp
+++ plug-ins/questreward/questring.cpp
@@ -20,103 +20,125 @@ void QuestRing::wear( Character *ch )
     ch->send_to( "{CТвое кольцо ярко вспыхивает.{x\r\n" );
 }
 
-void QuestRing::equip( Character *ch ) {
-    Affect *paf;
-    Affect af;
-    
+void QuestRing::equip( Character *ch ) 
+{
     obj->level = ch->getRealLevel( );
     
     if( obj->affected ) {
-        for( paf = obj->affected; paf; paf = paf->next )
-            addAffect( ch, paf );
+        // Updated existing affects to match player level.
+        Affect *paf;
+        for (paf = obj->affected; paf; paf = paf->next)
+            addAffect(ch, paf);
     }
     else {
+        // Add affects for the first time.
+        static const int applies [] = {
+            APPLY_INT, APPLY_WIS, APPLY_CON, APPLY_DEX, APPLY_STR, 
+            APPLY_AC, APPLY_HIT, APPLY_MANA, APPLY_MOVE,
+            APPLY_HITROLL, APPLY_DAMROLL, -1
+        };
+
+        Affect af;
+
         af.where = TO_OBJECT;
         af.type  = -1;
         af.duration = -1;
         af.bitvector = 0;
 
-        af.location = APPLY_INT;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_WIS;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_AC;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_HIT;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_MANA;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_MOVE;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_HITROLL;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
-
-        af.location = APPLY_DAMROLL;
-        addAffect( ch, &af );
-        affect_to_obj( obj, &af );
+        for (int i = 0; applies[i] != -1; i++) {
+            af.location = applies[i];
+            addAffect(ch, &af);
+            affect_to_obj(obj, &af);
+        }
     }
 }
 
-void QuestRing::addAffect( Character *ch, Affect *paf ) {
-  short level = ch->getModifyLevel();
+void QuestRing::addAffect( Character *ch, Affect *paf ) 
+{
+    short level = ch->getModifyLevel();
+    bool caster = ch->getTrueProfession( )->getFlags( ).isSet(PROF_CASTER);
+    bool fighter = !caster;
+    bool evil = IS_EVIL(ch);
+    bool good = IS_GOOD(ch);
+    bool neutral = IS_NEUTRAL(ch);
 
-  switch( paf->location ) {
-    case APPLY_DAMROLL:
-      paf->level = level;
-      paf->modifier = IS_EVIL( ch ) ? ( level / 5 ) :
-                        IS_NEUTRAL( ch ) ? ( level / 10 ) : ( level / 20 );
-      if (!ch->getTrueProfession( )->getFlags( ).isSet(PROF_CASTER)) // Bonus for battle clases
-          paf->modifier += paf->modifier/2;
-      return;
-    case APPLY_HITROLL:
-      paf->level = level;
-      paf->modifier = IS_EVIL( ch ) ? ( level / 20 ) :
-                        IS_NEUTRAL( ch ) ? ( level / 10 ) : ( level / 5 );
-      if (!ch->getTrueProfession( )->getFlags( ).isSet(PROF_CASTER)) // Bonus for battle clases
-          paf->modifier += paf->modifier/2;
-      return;
-    case APPLY_HIT:
-      paf->level = level;
-      paf->modifier = level * 3 / 2;
-      return;
-    case APPLY_MANA:
-      paf->level = level;
-      paf->modifier = level * 3 / 2;
-      if (ch->getTrueProfession( )->getFlags( ).isSet(PROF_CASTER)) // Bonus for caster clases
-           paf->modifier += paf->modifier*3/2;
-      return;
-    case APPLY_MOVE:
-      paf->level = level;
-      paf->modifier = level;
-      return;
-    case APPLY_INT:
-      paf->level = level;
-      paf->modifier = 1;
-      return;
-    case APPLY_WIS:
-      paf->level = level;
-      paf->modifier = 1;
-      return;
-    case APPLY_AC:
-      paf->level = level;
-      paf->modifier = IS_GOOD( ch ) ? -( level * 2 ) :
-                        IS_EVIL( ch ) ? -( level ) : -( level * 3 / 2 );
-      return;
-  }
+    int mod = 0;
 
+    switch( paf->location ) {
+        case APPLY_DAMROLL:
+            if (fighter) { // battle class
+                if (evil)
+                    mod = level / 10 + 3;
+                else if (neutral)
+                    mod = level / 12 + 2;
+                else
+                    mod = level / 14 + 1;
+              
+            } else { // caster class
+                if (evil)
+                    mod = level / 14 + 3;
+                else if (neutral)
+                    mod = level / 14 + 1;
+                else
+                    mod = level / 14;            
+            }
+            break;
+
+        case APPLY_HITROLL:
+            if (fighter) { // battle class
+                if (evil)
+                    mod = level / 20 + 1;
+                else if (neutral)
+                    mod = level / 12 + 1;
+                else
+                    mod = level / 10 + 3;
+              
+            } else { // caster class
+                if (evil)
+                    mod = level / 14;
+                else if (neutral)
+                    mod = level / 12;
+                else
+                    mod = level / 10;            
+            }
+            break;
+
+
+        case APPLY_HIT:
+            if (fighter) { // battle class
+                if (evil)
+                    mod = level / 2;
+                else if (neutral)
+                    mod = 3 * level / 4;
+                else
+                    mod = level;
+              
+            } else { // caster class
+                if (evil)
+                    mod = 3 * level / 2;
+                else if (neutral)
+                    mod = level * 2;
+                else
+                    mod = level * 3;            
+            }
+            break;
+
+        case APPLY_MANA:
+            if (fighter)  // battle class
+                mod = level;
+            else // caster class
+                mod = level * 3;
+            break;
+
+        case APPLY_MOVE:
+            mod = level;
+            break;
+
+        default:
+            return;
+    }
+
+    paf->level = level;
+    paf->modifier = mod;
 }
-
 
