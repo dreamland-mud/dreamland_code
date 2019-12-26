0647114b430210bfcf0f4a3be3ce577098f0da23
Ruffina Koza <ruffina.koza@gmail.com>
2019-12-26 20:44:16 +1300

    Fenia: disable servlet until auth is ready

--- plug-ins/feniaroot/evalservlet.cpp
+++ plug-ins/feniaroot/evalservlet.cpp
@@ -11,7 +11,7 @@ SERVLET_HANDLE(fenia_eval, "/eval")
     try {
         if(request.method != "POST")
             throw ::Exception("Expecting POST method");
-
+#if 0
         CodeSource &cs = CodeSource::manager->allocate();
         
         cs.author = "Filths";
@@ -19,10 +19,12 @@ SERVLET_HANDLE(fenia_eval, "/eval")
 
         cs.content = request.body;
         Register rc = cs.eval(Register());
-
+#endif
         response.status = 200;
         response.message = "OK";
+#if 0
         response.body = rc.toString();
+#endif
     
     } catch (const ::Exception &e) {
         response.status = 500;
