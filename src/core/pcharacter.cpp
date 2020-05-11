b47fd5cdb8a803afa38409efd2ef9c76f1bd91ca
Kit Oliynyk <22694346+fiorine@users.noreply.github.com>
2020-05-11 11:06:22 -0400

    translate default "Immortal" string

--- src/core/pcharacter.cpp
+++ src/core/pcharacter.cpp
@@ -142,7 +142,7 @@ void CachedNoun::clear( )
 void CachedNoun::update( PCharacter *ch )
 {
     static const DLString vampireName = "{DСоздани|е|я|ю|е|ем|и ночи{x";
-    static const DLString immortalName = "Immortal";
+    static const DLString immortalName = "{CБессмертный{x";
     MultiGender mg( ch->getSex( ), Number::SINGULAR );
     
     /* plain english name */
