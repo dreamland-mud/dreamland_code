741fef54bd8b61d7a05fd33f81a42710518977d1
Kerrad <krap87@gmail.com>
2020-05-11 13:16:08 +0200

    blackjack had 25 max in calculations, now depends on MAX_STAT

--- plug-ins/groups/class_thief.cpp
+++ plug-ins/groups/class_thief.cpp
@@ -1277,7 +1277,7 @@ SKILL_RUNP( blackjack )
         ch->setWait( gsn_blackjack->getBeats( ) );
 
         chance = ( int ) ( 0.5 * gsn_blackjack->getEffective( ch ) );
-        chance += URANGE( 0, ( ch->getCurrStat(STAT_DEX) - 20) * 2, 10);
+        chance += URANGE( 0, ( ch->getCurrStat(STAT_DEX) - BASE_STAT) * 2, (MAX_STAT-BASE_STAT) * 2);
         chance += victim->can_see(ch) ? 0 : 5;
         if (victim->is_npc( )
             && victim->getNPC( )->behavior
