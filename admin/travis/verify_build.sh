ae90e48608d16de6253c50176f727428e2922dd4
Ruffina Koza <ruffina.koza@gmail.com>
2020-05-15 20:01:20 +1200

    Travis: clean up redundant packages, split build into stages

--- admin/travis/verify_build.sh
+++ admin/travis/verify_build.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
-set -e
-
 # Normally /home/travis/build/dreamland-mud/dreamland_code
 ROOT=`pwd`
 
-echo ">>> Building DreamLand in $ROOT"
+travis_before_script() {
+    mkdir -p share && \
+    cd share && \
+    git clone https://github.com/dreamland-mud/dreamland_world.git DL
+}
 
-echo ">>> Installing dependency packages..."
-sudo apt-get update
-sudo apt-get install -y git g++ gcc make automake libtool bison flex libfl-dev bzip2
-sudo apt-get install -y libcrypto++-dev libjsoncpp-dev libdb5.3 libdb5.3-dev libdb5.3++ libdb5.3++-dev zlib1g zlib1g-dev libssl-dev db-util
+travis_script() {
+    pwd
+    ls
+    mkdir -p objs && \
+    make -f Makefile.git && \
+    cd objs && \
+    ../configure --prefix=$ROOT && \
+    make -j 2 && make install
+} 
 
-echo ">>> Cloning dreamland_world..."
-mkdir -p share && \
-cd share && \
-git clone https://github.com/dreamland-mud/dreamland_world.git DL
-
-echo ">>> Configure and build the sources..."
-cd $ROOT && \
-mkdir -p objs && \
-make -f Makefile.git && \
-cd objs && \
-../configure --prefix=$ROOT && \
-make -j 2 && make install && \
-echo ">>> Installation complete."
-
-echo ">>> Starting and shutting down DreamLand..."
-cd $ROOT && \
-./bin/dreamland admin/travis/dreamland.xml
-echo ">>> All done."
+travis_after_success() {
+    echo ">>> Starting and shutting down DreamLand..."
+    ./bin/dreamland admin/travis/dreamland.xml
+    echo ">>> All done."
+}
 
