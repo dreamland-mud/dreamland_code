7c4e011c6186b01fa6453789f59032c4d3dee335
Ruffina Koza <ruffina.koza@gmail.com>
2021-02-28 21:20:47 +1300

    Fenia: enable autosave for more subj patterns

--- plug-ins/feniaroot/codesourcerepo.cpp
+++ plug-ins/feniaroot/codesourcerepo.cpp
@@ -26,13 +26,19 @@ CodeSourceRepo::CodeSourceRepo()
     subjPatterns.push_back(
         RegExp::Pointer(NEW, "^(spell/[a-z ]{2,})/(run[a-zA-Z]+)$", true));
     subjPatterns.push_back(
-        RegExp::Pointer(NEW, "^(areas/[a-z0-9]{2,}.are/[a-z]+)/([0-9.a-zA-Z ]+)$", true));
+        RegExp::Pointer(NEW, "^(areas/[-a-z0-9]{2,}.are/[a-z]+)/([0-9.a-zA-Z ]+)$", true));
     subjPatterns.push_back(
-        RegExp::Pointer(NEW, "^(areas/[a-z0-9]{2,}.are)/([a-z]+)$", true));
+        RegExp::Pointer(NEW, "^(areas/[-a-z0-9]{2,}.are)/([a-z]+)$", true));
     subjPatterns.push_back(
         RegExp::Pointer(NEW, "^(quest)/([0-9.a-zA-Z :]+)$", true));
     subjPatterns.push_back(
         RegExp::Pointer(NEW, "^(gquest)/([0-9.a-zA-Z :]+)$", true));
+    subjPatterns.push_back(
+        RegExp::Pointer(NEW, "^(behavior)/([0-9.a-zA-Z ]+)$", true));
+    subjPatterns.push_back(
+        RegExp::Pointer(NEW, "^(global/on[A-Z][a-z]+)/([_a-zA-Z]+)$", true));
+    subjPatterns.push_back(
+        RegExp::Pointer(NEW, "^(global/post[A-Z][a-z]+)/([_a-zA-Z]+)$", true));
 }
 
 CodeSourceRepo::~CodeSourceRepo() 
