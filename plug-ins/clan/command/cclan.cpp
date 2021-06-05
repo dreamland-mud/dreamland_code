faa1e9a8f86ddc3d8dbe7959bd78c211e7a1438b
Kit Oliynyk <22694346+fiorine@users.noreply.github.com>
2021-06-05 13:32:55 -0400

    improved clan messaging
    - russian name on petition accept/induct
    - special message on clan NONE (dispersed)

--- plug-ins/clan/command/cclan.cpp
+++ plug-ins/clan/command/cclan.cpp
@@ -1324,7 +1324,10 @@ void CClan::doInduct( PCMemoryInterface *victim, const Clan &clan )
         PCharacterManager::saveMemory( victim );
     
     if (victim->getLevel() <= LEVEL_MORTAL) {
-        DLString what = fmt(0, "{W%s вступает в %s.{x", victim->getName().c_str(), clan.getRussianName( ).ruscase('4').c_str());
+        if (clan != "NONE")
+            DLString what = fmt(0, "{W%1$^C1 вступает в %s.{x", victim, clan.getRussianName( ).ruscase('4').c_str());
+        else
+            DLString what = fmt(0, "{W%1$^C1 становится внекланов%1$Gым|ым|ой.{x", victim);
         infonet(victim->getPlayer(), 0, "{CТихий голос из $o2: ", what.c_str());
         send_discord_clan(what);
         send_telegram(what);
