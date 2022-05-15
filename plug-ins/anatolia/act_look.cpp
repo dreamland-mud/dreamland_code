f451245c02207c612f5c7757e4a9f30c03a64dc4
Ruffina Koza <ruffina.koza@gmail.com>
2022-05-15 17:20:34 +1200

    Fix bug when vampires saw everyone else as '(Undead)

--- plug-ins/anatolia/act_look.cpp
+++ plug-ins/anatolia/act_look.cpp
@@ -671,10 +671,10 @@ void show_char_to_char_0( Character *victim, Character *ch )
         buf << fmt(ch, "({1{mОчарован%Gо||а{2)", victim);
 
     if (CAN_DETECT(ch, DETECT_UNDEAD)) {
-		if (victim->is_npc() &&
-			(IS_SET(victim->act, ACT_UNDEAD) || IS_SET(victim->form, FORM_UNDEAD)) )
-			buf << "({1{rНежить{2)";
-		else if (!victim->is_npc() && IS_VAMPIRE(ch))
+        bool npcUndead = victim->is_npc() && 
+            (IS_SET(victim->act, ACT_UNDEAD) || IS_SET(victim->form, FORM_UNDEAD));
+    
+		if (npcUndead || IS_VAMPIRE(victim))
 			buf << "({1{rНежить{2)";
 	}      
 
