e8bce6d8c8133b8cda7b9c01e04446d56c59632d
Ruffina Koza <ruffina.koza@gmail.com>
2024-10-27 22:59:10 +1300

    Substitute UA apostrophe sign (U+2019) with single quotation mark

--- src/util/iconvmap.cpp
+++ src/util/iconvmap.cpp
@@ -22,12 +22,31 @@ IconvMap::~IconvMap()
 }
 
 std::string 
-IconvMap::operator() (const std::string &src)
+IconvMap::operator() (const std::string &constSrc)
 {
+    string src = constSrc;
     icnv_desc = iconv_open(to, from);
 
     if (icnv_desc == (iconv_t)(-1)) {
         throw new Exception("Failed to iconv_open");
+    }
+
+    // Substitute UA apostrophe sign (U+2019) with single quotation mark,
+    // because there's no corresponding character in KOI8-U or CP1251 encoding.
+    if (from == string("utf-8")) {
+        const string modifiedLetterApostrophe = "\xCA\xBC";
+        const string rightSingleQuotationMark = "'";
+        size_t index = 0;
+        
+        while (true) {
+            /* Locate the substring to replace. */
+            index = src.find(modifiedLetterApostrophe, index);
+            if (index == string::npos) 
+                break;
+
+            /* Make the replacement. */
+            src = src.replace(index, modifiedLetterApostrophe.length(), rightSingleQuotationMark);
+        }
     }
 
     char *in = (char*) src.c_str();
